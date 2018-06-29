9c7bcf3652cfd0b04033560fdd03e5071a6e5afb
Saba Turdzeladze <sturd2015@agruni.edu.ge>
2018-06-29 23:56:07 +0400

    finished DOM_Exercise application

--- Exercises/DOM/app.js
+++ Exercises/DOM/app.js
@@ -1,3 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+
 // var titles = document.getElementsByClassName('title');
 
 // console.log(Array.isArray(titles));
@@ -100,3 +103,48 @@ addForm.addEventListener('submit', function(e){
     list.appendChild(li);
 });
 
+// hide books
+
+const hideBox = document.querySelector('#hide');
+
+hideBox.addEventListener('change', function(e){
+    if (hideBox.checked){
+        list.style.display = 'none';
+    } else {
+        list.style.display = 'initial';
+    }
+});
+
+// filter books
+const searchBar = document.forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', function(e){
+    const term = e.target.value.toLowerCase();
+    const books = list.getElementsByTagName('li');
+    Array.from(books).forEach(function(book){
+        const title = book.firstElementChild.textContent;
+        if (title.toLowerCase().indexOf(term) != -1){   // if search doesn't match any of the names indexOf() returns -1
+            book.style.display = 'block';
+        }else {
+            book.style.display = 'none';
+        }
+    })
+})
+
+
+//tabbed content
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+tabs.addEventListener('click', function(e){
+    if (e.target.tagName == 'LI'){
+        const targetPanel = document.querySelector(e.target.dataset.target);
+        panels.forEach(function(panel){
+            if (panel == targetPanel){
+                panel.classList.add('active');
+            }else {
+                panel.classList.remove('active');
+            }
+        })
+    }
+})
+
+});
