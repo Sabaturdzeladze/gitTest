be80afc53327092f3fd6e3b80de5eec34e393b18
Sabaturdzeladze <sabaturdzeladze@gmail.com>
2018-07-28 16:45:46 +0400

    promise, fetch, race, promise.all, async/await

---
+++ 28_july_FrontEnd_2018/js/main.js
@@ -0,0 +1,54 @@
+function makePromise(time) {
+    return new Promise( function(resolve, reject){
+            setTimeout( () => {
+                if(time > 3000) {
+                    // console.log(`rejecting`);
+                    reject(new Error(`Too long time`));
+                }
+                else {
+                    // console.log(`Resolving`);
+                    resolve(`OK its ${time}`);
+                }
+            }, time)
+    }  );
+}
+
+// let sec1 = makePromise(1000)
+// sec1.then( result => {
+//     console.log(result);
+// } )
+// .catch( error => console.log(error.message ))
+
+// let sec2 = makePromise(2000)
+
+// let sec3 = makePromise(3000)
+
+// let sec4 = makePromise(4000)
+// sec4.then(result => {
+//     console.log(result);
+// } )
+// .catch( error => console.log(error.message ))
+
+let allPromise = Promise.all([sec1, sec2, sec3]);
+// allPromise
+// .then( result => {
+//     console.log(result);
+// } )
+// .catch( error => console.log(error.message) )
+
+// let allRace = Promise.race([sec1, sec2, sec3, sec4])
+// allRace
+// .then( result => {
+//     console.log(result);
+// } )
+// .catch( error => console.log(error.message) )
+
+// let resolvedPromise = Promise.resolve(`Resolved Promise Example`)
+
+// resolvedPromise.then( (result) => {
+
+// } )
+// .catch( err => console.log(err.message))
+
+
+// Async / Await
---
+++ 28_july_FrontEnd_2018/js/promise.js
@@ -0,0 +1,64 @@
+window.onload = () => {
+    let usersEndPoint = `https://jsonplaceholder.typicode.com/users`
+
+fetch(usersEndPoint, {
+    method: `get`,
+})
+.then( response => {
+    if (response.status == 200){
+        return response.json();
+    }
+} )
+.then( usersList => {
+    document.body.innerHTML = usersList.map( user => {
+        // onclick="showDetails(${user.id})
+        return `<div data-user-id="${user.id}"">
+            <h2>${user.name} <small>id : ${user.id}</small></h2>
+        </div>`;
+    } ).join('')
+    let users = document.querySelectorAll(`[data-user-id]`);
+    users.forEach( user => {user.addEventListener('click', (event) => {
+        console.log(`I am an event ${event.target}`);
+        let userDiv = event.target;
+        if (userDiv.tagName.toLowerCase() !=`div`){
+            while ( userDiv.tagName.toLowerCase() != `div`){
+                userDiv = userDiv.parentNode
+            }
+        }
+        let userId = user.dataset.userId;
+        showDetails(userId)
+        console.log(userDiv.dataset);
+        
+    } )
+} )
+})
+
+.catch( err => console.log(`Error - ${err.message}`) )
+
+
+function showDetails(userId) {
+   fetch(`${usersEndPoint}/${userId}`, {
+       method: "get"
+   })
+   .then((response) =>{
+       if(response.status === 200){
+           return response.json();
+       }
+   })
+   .then(userDetail => {
+        document.body.innerHTML = `
+        <div class="user__detail--item">   
+            <h2>${userDetail.name}</h2>
+            <h3>home - ${userDetail.home}</h3>
+            <h3>phone - ${userDetail.phone}</h3>
+            <h3>email - ${userDetail.email}</h3>
+        </div>
+        `
+        
+   })
+   .catch((error) => {
+       console.log(`Error  - ${error.message}`)
+   })
+    }
+}
+
