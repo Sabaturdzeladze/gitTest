dfadac70460568afeab93a234708d6cb2aa543f8
Saba Turdzeladze <sturd2015@agruni.edu.ge>
2018-09-21 01:00:54 +0400

    collision, gravitation

--- Canvas/js/canvas.js
+++ Canvas/js/canvas.js
@@ -51,24 +51,45 @@ let minRadius = 2;
 
 let colorArray = ['#010A26', '#011640', '#B6D6F2', '#FFFFFF', '#E83338']
 
-// window.addEventListener('mousemove', function (event) {
-//     mouse.x = event.x;
-//     mouse.y = event.y;
+let gravity = 1;
+let friction = 0.99;
 
-// })
+window.addEventListener('mousemove', function (event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+})
 
-// window.addEventListener('resize', function () {
-//     canvas.width = window.innerWidth;
-//     canvas.height = window.innerHeight;
-//     init();
-// })
+window.addEventListener('resize', function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    init();
+})
+
+addEventListener('click', () => {
+    init()
+})
+
+function randomIntFromRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function randomColor(colors) {
+    return colorArray[Math.floor(Math.random() * colors.length)];
+}
+
+function getDistance (x1, y1, x2, y2){
+    let xDistance = x2 - x1;
+    let yDistance = y2 - y1;
+    
+    return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+}
 
 
-function Ball(x, y, dy, radius, color) {
+function Circle(x, y, /* dx, dy, */ radius, color) {
     this.x = x;
     this.y = y;
     // this.dx = dx;
-    this.dy = dy;
+    // this.dy = dy;
     this.radius = radius;
     // this.minRadius = radius;
     // this.color = colorArray[Math.floor(Math.random() * (colorArray.length))];
@@ -81,6 +102,8 @@ function Ball(x, y, dy, radius, color) {
         c.fillStyle = this.color;
         c.stroke();
         c.fill(); // circle color
+        c.stroke();
+        c.closePath()
     }
 
     this.update = function () {
@@ -105,20 +128,28 @@ function Ball(x, y, dy, radius, color) {
         //     this.radius -= 1;
         // }
         // #endregion
+        // if (this.y + this.radius + this.dy > canvas.height) {
+        //     this.dy = -this.dy * friction;
+        // } else {
+        //     this.dy += gravity;
+        // }
 
-        if (this.y + this.radius > canvas.height){
-            this.dy = -this.dy;
-        }
-        this.y += this.dy 
+        // if (this.x + this.radius + this.dx > canvas.width || this.x - this.radius <= 0) {
+        //     this.dx = -this.dx;
+        // }
+
+        // this.y += this.dy
+        // this.x += this.dx
         this.draw()
     }
 }
 
 
-let circleArray = [];
-var ball;
+// let circleArray = [];
+// var ball;
+// var ballArray = [];
+let circle1, circle2
 function init() {
-    ball = new Ball(canvas.width / 2, canvas.height / 2, 2, 30, 'red')
     // circleArray = [];
     // for (let i = 0; i < 1000; i++) {
     //     let radius = Math.random() * 3 + 1;
@@ -128,6 +159,22 @@ function init() {
     //     let dy = (Math.random() - 0.5) * 2;
     //     circleArray.push(new Circle(x, y, dx, dy, radius))
     // }
+
+    // ballArray = [];
+
+    // for (let i = 0; i < 500; i++) {
+    //     let radius = randomIntFromRange(8, 20);
+    //     let x = randomIntFromRange(radius, canvas.width - radius)
+    //     let y = randomIntFromRange(0, canvas.height - radius)
+    //     let dx = randomIntFromRange(-2, 2)
+    //     let color = randomColor(colorArray)
+    //     ballArray.push(new Ball(x, y, dx, 2, radius, color))
+    // }
+
+
+    //collision
+    circle1 = new Circle(300, 300, 100, 'black');
+    circle2 = new Circle(undefined, undefined, 30, 'red');
 }
 
 
@@ -136,8 +183,24 @@ function init() {
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth, innerHeight);
-    ball.update();
-    
+    // ball.update();
+
+    // for (let ball of ballArray) {
+    //     ball.update()
+    // }
+
+    //collision
+    circle1.update();
+    circle2.x = mouse.x;
+    circle2.y = mouse.y;
+    circle2.update();
+
+    if (getDistance(circle1.x, circle1.y, circle2.x, circle2.y) <= circle1.radius + circle2.radius){
+        circle1.color = 'red';
+    }
+    else {
+        circle1.color = 'black';
+    }
 }
 init()
 animate();
