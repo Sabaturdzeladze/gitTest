72b31c3865080b2283120358d8a2ec9faf1d3077
Sabaturdzeladze <sturd2015@agruni.edu.ge>
2018-10-05 21:31:51 +0400

    creating server with nodeman (using postman)

---
+++ 5_oct_FrontEnd/server.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`Hello`);
+})
+
+app.get('/users/:userId(\\d)', (req, res) => {
+    const userId = req.params.userId;
+    res.send(`user id - ${userId}`)
+})
+
+let id = 0;
+
+const products = [
+    {name: 'apple', id: id++, price: 3},
+    {name: 'grape', id: id++, price: 5}
+]
+
+// query string --------- '?'
+app.get('/products', (req, res) => {
+    // res.send(`
+    //     <h1>Welcome</h1>
+    //     <h2>${req.query.name} ${req.query.lastName}</h2>
+    // `);
+
+    res.send(
+        products.map( product => {
+            return `<h2 id="product-${product.id}" data-id="${product.id}" 
+            onclick="window.location.replace('http://localhost:3000/products/${product.id}')">${product.name}</h2>`
+        }).join(' ')
+    )
+})
+
+app.get('/products/:prodId', (req, res) => {
+    let id = req.params.prodId
+    const product = products.find(prod => prod.id == id);
+
+    let response = 'Product not Found';
+    if (product){
+        res.send(`
+            <a href='http://localhost:3000/products'>Home</a>
+            <h1>${product.name}</h1>
+            <hr>
+            <h3>$ ${product.price}</h3>
+        `)
+    }
+    res.send(response)
+})
+
+app.post('/products/:id/:name/:price', (req, res) => {
+    let product = {
+        id: req.params.id,
+        name: req.params.name,
+        price: req.params.price
+    }
+
+    products.push(product);
+    res.json( {message: 'product', product} );
+})
+
+
+app.delete('/products/:id', (req, res) => {
+    const id = products.findIndex(p => p.id == req.params.id)
+    const product = products[id];
+    products.splice(id, 1);
+    res.json({message: 'product deleted', product});
+})
+
+app.listen(3000);
+// localhost:3000/users/5?age=14&grade=10
