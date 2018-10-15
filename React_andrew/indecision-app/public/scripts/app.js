cb6dd6744420cf08421d1e205fa5f056e74c4f99
Sabaturdzeladze <sturd2015@agruni.edu.ge>
2018-10-15 16:34:47 +0400

    React exercise: initial commit

---
+++ React_andrew/indecision-app/public/scripts/app.js
@@ -0,0 +1,56 @@
+'use strict';
+
+// JSX - Javascript XML
+
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
+);
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Saba Turdzeladze'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: 26'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: Tbilisi'
+    )
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(templateTwo, appRoot);
