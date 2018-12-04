98329d305d04af7ab19f039728dc4d3e2cc4019d
Sabaturdzeladze <sturd2015@agruni.edu.ge>
2018-12-04 18:22:39 +0400

    Dashboard Part-1 (create-profile)

--- MERN_traversy/client/src/App.js
+++ MERN_traversy/client/src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { clearCurrentProfile } from "./actions/profileActions";
 
 import { Provider } from "react-redux";
 import store from "./store";
+
+import PrivateRoute from "./components/common/PrivateRoute";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Dashboard from "./components/dashboard/Dashboard";
 
 import "./App.css";
+import CreateProfile from "./components/create-profile/CreateProfile";
 
 // check for token
 if (localStorage.jwtToken) {
@@ -29,10 +34,11 @@ if (localStorage.jwtToken) {
   if (decoded.exp < currentTime) {
     // Logout user
     store.dispatch(logoutUser());
-    // TODO: Clear current Profile
+    // Clear current Profile
+    store.dispatch(clearCurrentProfile());
     // Redirect to login
-    window.location.href = '/login';
-  } 
+    window.location.href = "/login";
+  }
 }
 
 class App extends Component {
@@ -46,6 +52,12 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/create-profile" component={CreateProfile} />
+              </Switch>
             </div>
             <Footer />
           </div>
