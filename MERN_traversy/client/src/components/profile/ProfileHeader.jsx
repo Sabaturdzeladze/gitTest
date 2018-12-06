c605245d9415808d719455bdaa6b064f83f96fd5
Sabaturdzeladze <sturd2015@agruni.edu.ge>
2018-12-06 14:32:29 +0400

    warning fix

--- MERN_traversy/client/src/components/profile/ProfileHeader.jsx
+++ MERN_traversy/client/src/components/profile/ProfileHeader.jsx
@@ -30,6 +30,7 @@ export default class ProfileHeader extends Component {
                     className="text-white p-2"
                     href={profile.website}
                     target="_blank"
+                    rel="noopener noreferrer"
                   >
                     <i className="fas fa-globe fa-2x" />
                   </a>
@@ -39,6 +40,7 @@ export default class ProfileHeader extends Component {
                     className="text-white p-2"
                     href={profile.social.twitter}
                     target="_blank"
+                    rel="noopener noreferrer"
                   >
                     <i className="fab fa-twitter fa-2x" />
                   </a>
@@ -48,6 +50,7 @@ export default class ProfileHeader extends Component {
                     className="text-white p-2"
                     href={profile.social.facebook}
                     target="_blank"
+                    rel="noopener noreferrer"
                   >
                     <i className="fab fa-facebook fa-2x" />
                   </a>
@@ -57,6 +60,7 @@ export default class ProfileHeader extends Component {
                     className="text-white p-2"
                     href={profile.social.linkedin}
                     target="_blank"
+                    rel="noopener noreferrer"
                   >
                     <i className="fab fa-linkedin fa-2x" />
                   </a>
@@ -66,6 +70,7 @@ export default class ProfileHeader extends Component {
                     className="text-white p-2"
                     href={profile.social.youtube}
                     target="_blank"
+                    rel="noopener noreferrer"
                   >
                     <i className="fab fa-youtube fa-2x" />
                   </a>
@@ -75,6 +80,7 @@ export default class ProfileHeader extends Component {
                     className="text-white p-2"
                     href={profile.social.instagram}
                     target="_blank"
+                    rel="noopener noreferrer"
                   >
                     <i className="fab fa-instagram fa-2x" />
                   </a>
